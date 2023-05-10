f5c6dc4d75f4376cedb574de3696c19693ec1dcd
MarvinElRhumAdorator <marvingilly@yahoo.fr>
2023-05-10 16:51:15 +0200

    S_negative_conditional

--- src/main/java/fr/kaibee/cleancode/conditional/ConditionalExamples.java
+++ src/main/java/fr/kaibee/cleancode/conditional/ConditionalExamples.java
@@ -4,7 +4,7 @@ public class ConditionalExamples {
 
     public void someMethod(Item[] items) {
         for (int i = 0; i < items.length; i++) {
-            if (!isNotAgedBrie(items[i])) {
+            if (isAgedBrie(items[i])) {
                 System.out.println("I'm aged brie");
             } else {
                 System.out.println("I'm not aged brie");
@@ -13,9 +13,10 @@ public class ConditionalExamples {
 
     }
 
-    private static boolean isNotAgedBrie(Item items) {
-        return !items.name.equals("Aged Brie");
+    private static boolean isAgedBrie(Item items) {
+        return items.name.equals("Aged Brie");
     }
+
 }
 
 class Item {
